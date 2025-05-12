eca3ce879589ecbc525a4e31c56d02173f3849dc
Proxuu <kacper2019114@gmail.com>
2025-05-12 19:51:30 +0200

    Create systemlogowania.py

---
+++ systemlogowania/systemlogowania.py
@@ -0,0 +1,79 @@
+uzytkownicy = {}
+
+def rejestracja():
+
+    login = input("Podaj nowy login: ")
+
+    if login in uzytkownicy:
+        print("Taki login już istnieje. Spróbuj ponownie")
+        return
+    
+    haslo = input("Podaj nowe haslo: ")
+
+    uzytkownicy[login] = {"hasło": haslo, "logowania": 0}
+    print("Zarejestrowano pomyśllnie")
+
+def logowanie():
+
+    login = input("Podaj login: ")
+
+    if login in uzytkownicy:
+        haslo = input("Podaj hasło: ")
+
+        if haslo in uzytkownicy[login]['hasło']:
+
+            uzytkownicy[login]["logowania"] += 1
+            panel_uzytkownika(login)
+    else:
+        print("Taki login nie istnieje! Spróbuj ponownie.")
+        return
+    
+
+
+def panel_uzytkownika(login):
+
+
+    while True:
+
+        print(f"\n--- PANEL UŻYTKOWNIKA: {login} ---")
+        print('[1] Pokaż liczbę logowań')
+        print('[2] Zmień hasło')
+        print('[3] Wyloguj się')
+
+        wybor = input("> ")
+
+        if wybor == "1":
+            print(f'\nLiczba logowań: {uzytkownicy[login]["logowania"]}')
+        elif wybor == "2":
+            nowe_haslo = input("Wprowadź nowe hasło: ")
+            uzytkownicy[login]["haslo"] = nowe_haslo
+            print("Hasło zmienione pomyślnie")
+        elif wybor == "3":
+            print("Dowidzenia!")
+            break
+        else:
+            print("Błąd")
+
+def main():
+    
+    while True:
+        print("\nWitaj w systemie!")
+        print("[1] Zaloguj się")
+        print("[2] Zarejestruj się")
+        print("[3] Zakończ")
+
+        wybor = input("> ")
+
+        if wybor == "1":
+            logowanie()
+        elif wybor == "2":
+            rejestracja()
+        elif wybor == "3":
+            break
+        else:
+            print("Błąd")
+
+main()
+
+
+    
